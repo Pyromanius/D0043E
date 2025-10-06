24a0d65a7f37ca63638d17bb924c7f99b53c283a
Pyromanius <Pyromanius@protonmail.com>
2025-10-06 11:46:35 +0200

    3g is now complete

--- Assignments/3g/answer.py
+++ Assignments/3g/answer.py
@@ -1,36 +1,36 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
 
+
 def multiply(a, b):
     result = b
-    for i in range (0, a):
+    for i in range(1, a):
         result = result + b
     return result
 
-def divide(a, b):   
+
+def divide(a, b):
     if b == 0:
         return "Error: Division by zero"
     return a / b
 
-def get_numbers():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
 
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 operation = input("Which operation would you like to perform? (+, -, *, /): ")
 
-if not operation in ['+', '-', '*', '/']:
+if operation not in ['+', '-', '*', '/']:
     print("Invalid operation")
 else:
-    print("The result is: ")
     if operation == '+':
-        get_numbers
+        print(f"The result of {a} + {b} is {add(a, b)}")
     elif operation == '-':
-        get_numbers
+        print(f"The result of {a} - {b} is {subtract(a, b)}")
     elif operation == '*':
-        get_numbers
+        print(f"The result of {a} * {b} is {multiply(a, b)}")
     elif operation == '/':
-        get_numbers
-
+        print(f"The result of {a} / {b} is {divide(a, b)}")
